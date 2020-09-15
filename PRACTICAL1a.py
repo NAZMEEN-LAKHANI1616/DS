03dedb5dfb56fd6e72d6f658b31b14eee9e1586c
NAZMEEN-LAKHANI1616 <60962705+NAZMEEN-LAKHANI1616@users.noreply.github.com>
2020-09-15 17:06:52 +0530

    PRACTICAL NO 1A

---
+++ PRACTICAL1a.py
@@ -0,0 +1,110 @@
+#sorting
+numbers = [7, 3, 2, 5] 
+# Sorting list of Integers 
+numbers.sort() 
+print(numbers)
+#searching
+def binarySearch(arr, low, high, key):
+    if (high < low):
+        return -1
+    mid = (low + high)/2
+    if (key == arr[int(mid)]):
+        return mid
+    if (key > arr[int(mid)]):
+        return binarySearch(arr,(mid + 1), high, key)
+    return (binarySearch(arr, low, (mid -1), key))
+arr = [5, 6, 7, 8, 9, 10]
+n = len(arr) 
+key = 10
+print("Index:", int(binarySearch(arr, 0, n, key) ))
+
+
+
+#reverse
+def reverseList(A, start, end):
+
+    while start < end:
+
+        A[start], A[end] = A[end], A[start]
+
+        start += 1
+
+        end -=1
+
+A = [1, 2, 3, 4, 5, 6]
+
+print(A)
+
+reverseList(A, 0, 5)
+
+print("Reversed list is")
+
+print(A)
+
+#merging
+def mergeArrays(arr1, arr2, n1, n2): 
+
+    arr3 = [None] * (n1 + n2) 
+
+    i = 0
+
+    j = 0
+
+    k =0
+
+    while i < n1 and j < n2: 
+
+        if arr1[i] < arr2[j]: 
+
+            arr3[k] = arr1[i] 
+
+            k = k + 1
+
+            i = i + 1
+
+        else: 
+
+            arr3[k] = arr2[j] 
+
+            k = k + 1
+
+            j = j + 1 
+
+    while i < n1: 
+
+        arr3[k] = arr1[i]; 
+
+        k = k + 1
+
+        i = i +1
+
+    while j < n2: 
+
+        arr3[k] = arr2[j]; 
+
+        k = k + 1
+
+        j = j + 1
+
+    print("Array after merging") 
+
+    for i in range(n1 + n2): 
+
+        print(str(arr3[i]), end = " ") 
+
+  
+
+arr1 = [1, 3, 5, 7] 
+
+n1 = len(arr1) 
+
+  
+
+arr2 = [2, 4, 6, 8] 
+
+n2 = len(arr2) 
+mergeArrays(arr1, arr2, n1, n2);
+
+
+
+
